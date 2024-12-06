93762cabf8cfadddec6369c92562e1a7e25035b3
Rutger Claes <rgc@rgc.be>
2024-12-06 19:50:28 +0000

    day 05: solution to part 2

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 use std::collections::{HashMap, HashSet};
 use std::convert::Infallible;
 use std::result::Result as StdResult;
-use std::usize;
+use std::str::FromStr;
 use tracing::Level;
 use utils::prelude::*;
 
@@ -11,7 +11,7 @@ fn main() -> Result<()> {
 
     let input = read_input()?;
     let mut lines = input.lines().peekable();
-    let rules: Vec<(usize, usize)> = lines
+    let rules: RuleSet = lines
         .peeking_take_while(|line| !line.is_empty())
         .map(|l| {
             l.split_once('|')
@@ -20,33 +20,95 @@ fn main() -> Result<()> {
         })
         .try_collect()?;
 
-    let updates: Vec<Vec<usize>> = lines
+    let updates: Vec<Update> = lines
         .skip_while(|l| l.is_empty())
-        .map(|l| l.split(',').map(|s| s.parse()).try_collect())
+        .map(|l| l.parse())
         .try_collect()?;
 
-    println!("{:?}", rules);
-    println!("{:?}", updates);
-
     print_part_1(part_one(&rules, &updates));
-    //print_part_2(part_two(&input));
+    print_part_2(part_two(&rules, &updates));
 
     Ok(())
 }
 
-#[tracing::instrument(level=Level::DEBUG,skip())]
-fn part_one( rules: &[(usize,usize)], updates: &Vec<Vec<usize>> ) -> StdResult<usize, Infallible> {
-    let rules: HashSet<(usize,usize)> = rules.iter().copied().collect();
-
-    Ok( updates.iter().filter( |update| { 
-        update.iter().combinations(2).all( |pair| {
-            let (a,b) = (*pair[0],*pair[1]);
-            rules.contains( &(a,b) )
+#[tracing::instrument(level=Level::DEBUG,skip(rules,updates))]
+fn part_one(rules: &RuleSet, updates: &[Update]) -> StdResult<usize, Infallible> {
+    Ok(updates
+        .iter()
+        .filter_map(|u| {
+            if u.is_valid(rules) {
+                Some(u.get_middle())
+            } else {
+                None
+            }
         })
-    } ).map( |update| update[ update.len() / 2]).sum())
+        .sum())
 }
 
-#[tracing::instrument(level=Level::DEBUG,skip())]
-fn part_two(input: &str) -> StdResult<usize, Infallible> {
-    todo!()
+#[tracing::instrument(level=Level::DEBUG,skip(rules,updates))]
+fn part_two(rules: &RuleSet, updates: &[Update]) -> Result<usize> {
+    updates
+        .iter()
+        .filter_map(|u| {
+            if u.is_valid(rules) {
+                None
+            } else {
+                // Find the element that has an equal number of elements in the before and after rules
+                Some(u.find_middle(rules).ok_or_else(|| {
+                    Error::SolutionNotFound(format!("did not find middle element in {u:?}"))
+                }))
+            }
+        })
+        .try_fold(0, |sum, e| e.map(|e| e + sum))
+}
+
+struct RuleSet(HashMap<usize, HashSet<usize>>);
+
+impl FromIterator<(usize, usize)> for RuleSet {
+    fn from_iter<T: IntoIterator<Item = (usize, usize)>>(iter: T) -> Self {
+        Self(iter.into_iter().into_grouping_map().collect())
+    }
+}
+impl RuleSet {
+    fn check_after(&self, first: &usize, second: &usize) -> bool {
+        self.0
+            .get(first)
+            .map(|after_first| after_first.contains(second))
+            .unwrap_or(false)
+    }
+}
+
+#[derive(Debug)]
+struct Update(Vec<usize>);
+
+impl FromStr for Update {
+    type Err = Error;
+
+    fn from_str(s: &str) -> StdResult<Self, Self::Err> {
+        let elem = s.split(',').map(|s| s.parse()).try_collect()?;
+        Ok(Update(elem))
+    }
+}
+
+impl Update {
+    fn is_valid(&self, rules: &RuleSet) -> bool {
+        self.0.iter().combinations(2).all(|pair| {
+            let (a, b) = (pair[0], pair[1]);
+            rules.check_after(a, b)
+        })
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn get_middle(&self) -> &usize {
+        &self.0[self.len() / 2]
+    }
+
+    fn find_middle(&self, rules: &RuleSet) -> Option<&usize> {
+        self.0
+            .iter()
+            .find(|i| self.0.iter().filter(|j| rules.check_after(i, j)).count() == self.len() / 2)
+    }
 }
